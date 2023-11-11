1a84b1d4e0bb2aeae304f7b9f6c4cc75e202e9ee
rajat89882 <143991789+rajat89882@users.noreply.github.com>
2023-11-11 17:47:08 +0530

    add

---
+++ js/gsap.js
@@ -0,0 +1,30 @@
+var tl = gsap.timeline();
+{
+  tl.from("header nav", {
+    y: -100,
+    duration: 0.5,
+    delay: 0.1,
+    opacity: 0,
+    stagger: 0.5,
+  });
+
+  tl.from(".banner .banner1", {
+    x: -100,
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0,
+    stagger: 0.5,
+  });
+
+  tl.from(".about ", {
+    x: -100,
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0,
+    stagger: 0.5,
+  });
+}
+
+
+
+

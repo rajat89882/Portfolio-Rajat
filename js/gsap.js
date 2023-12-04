268f2551d0c7052fc312d64144e26cfba21e0c9c
asthkr <rc8988293@gmail.com>
2023-12-04 15:00:54 +0530

    add

--- js/gsap.js
+++ js/gsap.js
@@ -1,25 +1,18 @@
 var tl = gsap.timeline();
 {
   tl.from("header nav", {
-    y: -100,
-    duration: 0.5,
+    y: -300,
+    duration: 0.6,
     delay: 0.1,
     opacity: 0,
     stagger: 0.5,
   });
 
   tl.from(".banner .banner1", {
-    x: -100,
-    duration: 0.5,
+    x: 0,
+    duration: 0.6,
     delay: 0.5,
-    opacity: 0,
-    stagger: 0.5,
-  });
-
-  tl.from(".about ", {
-    x: -100,
-    duration: 0.5,
-    delay: 0.5,
+    scale: 0.1,
     opacity: 0,
     stagger: 0.5,
   });
